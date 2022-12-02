ba58611e24275640197a54a51743326b5f9e4520
outman1998 <outmanmounir98@gmail.com>
2022-12-02 11:53:51 +0100

    added prework files to repo

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { HeroComponent } from '../components/hero/hero.component';
+
+import { NavComponent } from '../components/nav/nav.component';
+import { ContentOneComponent } from '../components/content-one/content-one.component';
+import { ContentTwoComponent } from '../components/content-two/content-two.component';
+import { FooterComponent } from '../components/footer/footer.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [
+    HomePage,
+    HeroComponent,
+    NavComponent,
+    ContentOneComponent,
+    ContentTwoComponent,
+    FooterComponent
+  ]
+})
+export class HomePageModule {}
