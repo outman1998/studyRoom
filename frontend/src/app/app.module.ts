9d9ce5a9831dab1e2f17dbfe690b7bca8c10c300
jacobgervin <jacobgervin@gmail.com>
2022-12-07 17:54:10 +0100

    Login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,9 +9,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { ForloebComponent } from './components/forloeb/forloeb.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [AppComponent, ForloebComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,     
+    AuthModule.forRoot({
+    domain: 'dev-l5ni5uzfmntgm850.us.auth0.com',
+    clientId: 'RpKts7o2RhyYXRUE3f8Xhxv9ialCMhSi'
+  })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
