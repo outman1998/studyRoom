2df14ccc5375add7f89c0ab259e23b50c2551893
jacobgervin <jacobgervin@gmail.com>
2022-12-20 00:38:54 +0100

    More nav

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public auth: AuthService) {}
 }
